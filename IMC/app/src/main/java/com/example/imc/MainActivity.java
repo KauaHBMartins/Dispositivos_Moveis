a4236463eddf71c19bd0a10bd0ad1ab84415cbb3
Kauã <kaua.bm@aluno.ifsc.edu.br>
2024-03-19 10:16:18 -0300

    ATT dnv

--- IMC/app/src/main/java/com/example/imc/MainActivity.java
+++ IMC/app/src/main/java/com/example/imc/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.imc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edPeso, edAltura;
+
+    double imc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edPeso = findViewById(R.id.edPeso);
+        edAltura = findViewById(R.id.edAltura);
+    }
+
+    public void calcularIMC(View view){
+        double peso = Double.parseDouble(edPeso.getText().toString());
+        double altura = Double.parseDouble(edAltura.getText().toString());
+        String imc = String.valueOf( peso / (altura*altura));
+        Log.d(imc, "calcularIMC: ");
+        Toast.makeText(this, imc, Toast.LENGTH_SHORT).show();
     }
 }
